c95951f197c6d954ec536c9085100fa5bec925ed
virtualuserx <virtualuserx@gmail.com>
2021-12-18 01:58:16 +1300

    Version 1.0.0

---
+++ src/app/products/discounter/discounter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discounter',
+  templateUrl: './discounter.component.html',
+  styleUrls: ['./discounter.component.css']
+})
+export class DiscounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
